f2af440fbb416d2544e3b0b7758c8c4fbd93ae11
subhan <subhan97ahmed@gmail.com>
2022-05-17 15:45:21 +0500

    created the following components Home.js NewPoll.js PollingStation.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Table, Container, Button } from "react-bootstrap"
 
 class Home extends Component {
     render() {
+        const prompList = [
+            "who will win election of 2022?",
+            "who will win election of 2022?",
+            "who will win election of 2022?",
+            "who will win election of 2022?",
+        ];
         return (
-            <div>
-                Home
-            </div>
+            <Container>
+                <Table style={{ margin: "5vh" }} striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>List of Polls</th>
+                            <th>Go to Poll</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            prompList.map((el, index) => {
+                                return (<tr key={index}>
+                                    <td>
+                                        {index + 1}
+                                    </td>
+                                    <td>
+                                        {el}
+                                    </td>
+                                    <td>
+                                        <Button>Go to Poll</Button>
+                                    </td>
+                                </tr>);
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </Container>
         );
     }
 }
